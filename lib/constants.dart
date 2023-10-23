cbcf589820217980862ce0b6577a66a57ad1052b
Mateus <mateus.venancio@basis.com.br>
2023-10-23 19:57:31 -0300

    initial configs + constants

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const spotifyAuthUrl = String.fromEnvironment('SPOTIFY_AUTH_URL');
+  static const spotifyPublicKey = String.fromEnvironment('SPOTIFY_PUBLIC_KEY');
+  static const spotifySecretKey = String.fromEnvironment('SPOTIFY_PRIVATE_KEY');
+}
